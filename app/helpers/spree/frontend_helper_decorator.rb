618a119be922ea47d066d7d45e113fd947766069
alexander mcdaid <alexander.mcdaid@gmail.com>
2017-02-14 19:47:06 +0000

    s

--- app/helpers/spree/frontend_helper_decorator.rb
+++ app/helpers/spree/frontend_helper_decorator.rb
@@ -1,10 +1,12 @@
-Spree::FrontendHelper.class_eval do
-  def taxon_group_tree(taxon_group, current_taxon)
-    content_tag :div, class: 'list-group' do
-      taxon_group.taxon_group_memberships.sort_by(&:position).map do |membership|
-        css_class = (current_taxon.eql? membership.taxon) ? 'list-group-item active' : 'list-group-item'
-        link_to(membership.taxon.name, seo_url(membership.taxon), class: css_class)
-      end.join("\n").html_safe
+module Spree
+  module FrontendHelper
+    def taxon_group_tree(taxon_group, current_taxon)
+      content_tag :div, class: 'list-group' do
+        taxon_group.taxon_group_memberships.sort_by(&:position).map do |membership|
+          css_class = (current_taxon.eql? membership.taxon) ? 'list-group-item active' : 'list-group-item'
+          link_to(membership.taxon.name, seo_url(membership.taxon), class: css_class)
+        end.join("\n").html_safe
+      end
     end
   end
 end
